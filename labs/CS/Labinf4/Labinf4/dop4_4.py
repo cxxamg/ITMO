bab8fc984481abe157c8348a59847e9dc402aeff
cxxamg <asokol200619@gmail.com>
2024-11-16 21:24:36 +0300

    Delete labs/CS/Labinf4/Labinf4 directory

--- labs/CS/Labinf4/Labinf4/dop4_4.py
+++
@@ -1,6 +0,0 @@
-import timeit
-
-tasks = ['labinf4', 'dop4_1', 'dop4_2', 'dop4_3']
-for task in tasks:
-    elapsed_time = timeit.timeit(f'import {task}', number=100)
-    print(f'{task} время: ', elapsed_time)
